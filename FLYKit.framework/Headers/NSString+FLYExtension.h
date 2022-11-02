fb7d9f741426e5016382066edf5cf612ad1611c8
“fly” <809176334@qq.com>
2022-11-02 17:26:44 +0800

    新增新增double转字符串方法

--- FLYKit.framework/Headers/NSString+FLYExtension.h
+++ FLYKit.framework/Headers/NSString+FLYExtension.h
@@ -12,6 +12,11 @@ NS_ASSUME_NONNULL_BEGIN
 @interface NSString (FLYExtension)
 
 
+/// double转字符串 (本方法不会精度丢失) (如果直接用stringValue或stringWithFormat来转换成String展示，很容易造成精度的丢失)
+/// @param number 数字
++ (NSString *)doubleToString:(double)number;
+
+
 /// 数组、字典转json字符串
 /// @param object 数组或字典
 /// @return string json字符串
