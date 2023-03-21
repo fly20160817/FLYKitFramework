a6472004397d22651f1af01a3e0b4fdb26e78fc7
“fly” <809176334@qq.com>
2023-03-21 14:42:42 +0800

    update

--- FLYKit.framework/Headers/FLYLabel.h
+++ FLYKit.framework/Headers/FLYLabel.h
@@ -9,20 +9,34 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-typedef NS_ENUM(NSInteger, FLYTextVerticalAlignment) {
-    FLYTextVerticalAlignmentTop = 1,
-    FLYTextVerticalAlignmentBottom,
+typedef NS_ENUM(NSInteger, FLYTextAlignment) {
+    FLYTextAlignmentTopLeft,
+    FLYTextAlignmentTopCenter,
+    FLYTextAlignmentTopRight,
+    FLYTextAlignmentCenterLeft,
+    FLYTextAlignmentCenter,
+    FLYTextAlignmentCenterRight,
+    FLYTextAlignmentBottomLeft,
+    FLYTextAlignmentBottomCenter,
+    FLYTextAlignmentBottomRight
 };
-
 
 @interface FLYLabel : UILabel
 
-/// 设置居上或者居下 （和系统的textAlignment并不冲突，比如可以设置居上，然后设置系统的居右，效果就是居右上）
-@property (nonatomic, assign) FLYTextVerticalAlignment textVerticalAlignment;
+//文本的对齐方式
+@property (nonatomic, assign) FLYTextAlignment flyTextAlignment;
 
-/// 文字距离label的边距 (和上面的textVerticalAlignment属性冲突，不能一起使用。但设置居左上、左下除外，并且文字不能超过label的大小才行)
-@property (nonatomic, assign) UIEdgeInsets textEdgeInset;
+//文本距离label边缘的内边距
+@property (nonatomic, assign) UIEdgeInsets textInsets;
+
+/*
+ 当使用系统的textAlignment时，textInsets设置的边距会出现问题。
+ 这是因为textAlignment属性是系统默认的属性，在系统内部实现中可能会使用与textInsets不兼容的布局算法，因此两个属性的组合可能会导致布局问题。
+ 为了避免这种问题，可以尽量使用自定义的flyTextAlignment属性来替代系统的textAlignment属性，这样可以确保textInsets属性的边距正确应用。
+ */
+
 
 @end
 
 NS_ASSUME_NONNULL_END
+
--- FLYKit.framework/Headers/UIView+FLYLayer.h
+++ FLYKit.framework/Headers/UIView+FLYLayer.h
@@ -11,51 +11,95 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface UIView (FLYLayer)
 
-/*
- 指定圆角 + 阴影
- 指定圆角 + 阴影 + 边框
- 
- 这两种做不了，直接问美工要切图吧。
- 注：可以自己先实现 指定圆角 或者 指定圆角+边框，然后让美工只切阴影，写个imageView把阴影显示出来。
- */
- 
-
 
 /// 设置圆角
-/// @param cornerRadius 圆角大小
-- (void)roundCorner:(CGFloat)cornerRadius;
+/// @param cornerRadius 圆角半径
+- (void)cornerRadius:(CGFloat)cornerRadius;
 
 /// 设置指定角的圆角 (设置完frame调用，或者在自动布局后面调用)
-/// @param cornerRadius 圆角大小
-/// @param rectCorner 哪些角
--(void)roundCorner:(CGFloat)cornerRadius rectCorners:(UIRectCorner)rectCorner;
+/// @param cornerRadius 圆角半径
+/// @param corners 需要设置为圆角的角
+-(void)cornerRadius:(CGFloat)cornerRadius roundCorners:(UIRectCorner)corners;
 
 /// 设置边框
 /// @param borderWidth 边框宽度
 /// @param borderColor 边框颜色
-- (void)border:(CGFloat)borderWidth color:(UIColor *)borderColor;
-
-/// 设置圆角和边框
-/// @param cornerRadius 圆角大小
-/// @param borderWidth 边框宽度
-/// @param borderColor 边框颜色
-- (void)roundCorner:(CGFloat)cornerRadius borderWidth:(CGFloat)borderWidth borderColor:(UIColor *)borderColor;
-
-/// 设置指定角的圆角和边框 (设置完frame调用，或者在自动布局后面调用)
-/// @param cornerRadius 圆角大小
-/// @param rectCorner 哪些角
-/// @param borderWidth 边框宽度
-/// @param borderColor 边框颜色
--(void)roundCorner:(CGFloat)cornerRadius rectCorners:(UIRectCorner)rectCorner borderWidth:(CGFloat)borderWidth borderColor:(UIColor *)borderColor;
+- (void)borderWidth:(CGFloat)borderWidth color:(UIColor *)borderColor;
 
 /// 设置阴影
 /// @param shadowColor 阴影颜色
 /// @param opacity 阴影透明度
-/// @param radius 阴影半径(默认3)
+/// @param radius 阴影半径
 /// @param offset 阴影偏移
--(void)shadow:(UIColor *)shadowColor opacity:(CGFloat)opacity radius:(CGFloat)radius offset:(CGSize)offset;
+-(void)shadowColor:(UIColor *)shadowColor opacity:(CGFloat)opacity radius:(CGFloat)radius offset:(CGSize)offset;
+
+
+
+/// 设置圆角 + 边框
+/// @param cornerRadius 圆角半径
+/// @param borderWidth 边框宽度
+/// @param borderColor 边框颜色
+- (void)cornerRadius:(CGFloat)cornerRadius borderWidth:(CGFloat)borderWidth borderColor:(UIColor *)borderColor;
+
+///  设置圆角 + 阴影
+/// @param cornerRadius 圆角半径
+/// @param shadowColor 阴影颜色
+/// @param shadowOpacity 阴影透明度
+/// @param shadowRadius 阴影半径
+/// @param shadowOffset 阴影偏移量
+- (void)cornerRadius:(CGFloat)cornerRadius shadowColor:(UIColor *)shadowColor shadowOpacity:(CGFloat)shadowOpacity shadowRadius:(CGFloat)shadowRadius shadowOffset:(CGSize)shadowOffset;
+
+/// 设置指定圆角 + 边框 (设置完frame调用，或者在自动布局后面调用)
+/// @param cornerRadius 圆角半径
+/// @param corners 需要设置为圆角的角
+/// @param borderWidth 边框宽度
+/// @param borderColor 边框颜色
+-(void)cornerRadius:(CGFloat)cornerRadius roundCorners:(UIRectCorner)corners borderWidth:(CGFloat)borderWidth borderColor:(UIColor *)borderColor;
+
+///  设置指定圆角 + 阴影 (设置完frame并且执行完addSubview再调用，或者在自动布局后面调用)
+/// @param cornerRadius 圆角半径
+/// @param corners 需要设置为圆角的角
+/// @param shadowColor 阴影颜色
+/// @param shadowOpacity 阴影透明度
+/// @param shadowRadius 阴影半径
+/// @param shadowOffset 阴影偏移量
+- (void)cornerRadius:(CGFloat)cornerRadius roundCorners:(UIRectCorner)corners shadowColor:(UIColor *)shadowColor shadowOpacity:(CGFloat)shadowOpacity shadowRadius:(CGFloat)shadowRadius shadowOffset:(CGSize)shadowOffset;
+
+/// 设置边框 + 阴影
+/// @param borderWidth 边框宽度
+/// @param borderColor 边框颜色
+/// @param shadowColor 阴影颜色
+/// @param shadowOpacity 阴影透明度
+/// @param shadowRadius 阴影半径
+/// @param shadowOffset 阴影偏移量
+-(void)borderWidth:(CGFloat)borderWidth borderColor:(UIColor *)borderColor shadowColor:(UIColor *)shadowColor shadowOpacity:(CGFloat)shadowOpacity shadowRadius:(CGFloat)shadowRadius shadowOffset:(CGSize)shadowOffset;
+
+
+
+/// 设置圆角 + 边框 + 阴影
+/// @param cornerRadius 圆角半径
+/// @param borderWidth 边框宽度
+/// @param borderColor 边框颜色
+/// @param shadowColor 阴影颜色
+/// @param shadowOpacity 阴影透明度
+/// @param shadowRadius 阴影半径
+/// @param shadowOffset 阴影偏移量
+-(void)cornerRadius:(CGFloat)cornerRadius borderWidth:(CGFloat)borderWidth borderColor:(UIColor *)borderColor shadowColor:(UIColor *)shadowColor shadowOpacity:(CGFloat)shadowOpacity shadowRadius:(CGFloat)shadowRadius shadowOffset:(CGSize)shadowOffset;
+
+/// 设置指定圆角 + 边框 + 阴影 (设置完frame并且执行完addSubview再调用，或者在自动布局后面调用)
+/// @param cornerRadius 圆角半径
+/// @param corners 需要设置为圆角的角
+/// @param borderWidth 边框宽度
+/// @param borderColor 边框颜色
+/// @param shadowColor 阴影颜色
+/// @param shadowOpacity 阴影透明度
+/// @param shadowRadius 阴影半径
+/// @param shadowOffset 阴影偏移量
+-(void)cornerRadius:(CGFloat)cornerRadius roundCorners:(UIRectCorner)corners borderWidth:(CGFloat)borderWidth borderColor:(UIColor *)borderColor shadowColor:(UIColor *)shadowColor shadowOpacity:(CGFloat)shadowOpacity shadowRadius:(CGFloat)shadowRadius shadowOffset:(CGSize)shadowOffset;
 
 
 @end
 
 NS_ASSUME_NONNULL_END
+
+
