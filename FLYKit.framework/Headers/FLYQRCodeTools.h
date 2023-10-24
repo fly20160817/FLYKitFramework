a7d9ac86ae9184ebfc0648e6c381a1d4f5b1d031
“fly” <809176334@qq.com>
2023-10-24 20:34:38 +0800

    update

--- FLYKit.framework/Headers/FLYQRCodeTools.h
+++ FLYKit.framework/Headers/FLYQRCodeTools.h
@@ -12,16 +12,16 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface FLYQRCodeTools : NSObject
 
-/// 从图片中读取二维码
+/// 从图片中读取二维码或条形码
 /// @param image 需要识别的image
-+ (NSArray<NSString *> *)readQRCodeWithImage:(UIImage *)image;
++ (NSArray<NSString *> *)readBarcodesWithImage:(UIImage *)image;
 
 
-/// 从相册中选取照片并获取二维码
+/// 从相册中选取照片并获取二维码或条形码
 /// (弹出照片选择器，选择一张照片，识别照片，返回识别结果，这一个方法一步完成。)
 /// (一张照片里可能包含多个二维码，所以返回的是数组，如果外界只要一个，直接.first或者.last取就可以了)
 /// @param completionBlock 获取二维码之后的回调
-+ (void)albumFetchQRCode:(void(^)(NSArray<NSString *> * codeStrings))completionBlock;
++ (void)albumFetchBarcodes:(void(^)(NSArray<NSString *> * codeStrings))completionBlock;
 
 
 /// 生成二维码
