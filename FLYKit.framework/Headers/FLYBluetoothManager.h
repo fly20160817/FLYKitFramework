b110314b013de3f584c45a2254d40780106f9969
“fly” <809176334@qq.com>
2024-07-18 12:10:35 +0800

    增加蓝牙特征通知状态发生变化时的回调

--- FLYKit.framework/Headers/FLYBluetoothManager.h
+++ FLYKit.framework/Headers/FLYBluetoothManager.h
@@ -62,16 +62,19 @@ NS_ASSUME_NONNULL_BEGIN
 
                     
 /** 扫描到服务时的回调 (内部会自动扫描特征。如果没有特殊需求，外界不需要实现此代理) */
--(void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(NSError *)error;
+-(void)peripheral:(CBPeripheral *)peripheral didDiscoverServices:(nullable NSError *)error;
 
 /** 扫描到特征时的回调 (内部会开启特征值的通知。如果没有特殊需求，外界不需要实现此代理)(如果需要关闭通知，外界实现此代理，在代理中关闭即可) */
--(void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(NSError *)error;
+-(void)peripheral:(CBPeripheral *)peripheral didDiscoverCharacteristicsForService:(CBService *)service error:(nullable NSError *)error;
 
 /** 特征里的值更新时回调 */
--(void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error;
+-(void)peripheral:(CBPeripheral *)peripheral didUpdateValueForCharacteristic:(CBCharacteristic *)characteristic error:(nullable NSError *)error;
 
 /** 写入数据后的回调 */
-- (void)peripheral:(CBPeripheral *)peripheral didWriteValueForCharacteristic:(CBCharacteristic *)characteristic error:(NSError *)error;
+- (void)peripheral:(CBPeripheral *)peripheral didWriteValueForCharacteristic:(CBCharacteristic *)characteristic error:(nullable NSError *)error;
+
+/** 通知状态发生变化时回调 */
+- (void)peripheral:(CBPeripheral *)peripheral didUpdateNotificationStateForCharacteristic:(CBCharacteristic *)characteristic error:(nullable NSError *)error;
 
 @end
 
@@ -144,5 +147,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 
 NS_ASSUME_NONNULL_END
+
+
 
 
