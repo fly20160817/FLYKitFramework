2efa66afe268c88c80198a91aaac0af5883416ed
“fly” <809176334@qq.com>
2024-07-26 10:37:36 +0800

    照片选择器改成单利，方便使用

--- FLYKit.framework/Headers/FLYKit-Swift.h
+++ FLYKit.framework/Headers/FLYKit-Swift.h
@@ -305,6 +305,8 @@ typedef unsigned int swift_uint4  __attribute__((__ext_vector_type__(4)));
 
 SWIFT_CLASS("_TtC6FLYKit14FLYImagePicker")
 @interface FLYImagePicker : NSObject
+SWIFT_CLASS_PROPERTY(@property (nonatomic, class, readonly, strong) FLYImagePicker * _Nonnull shared;)
++ (FLYImagePicker * _Nonnull)shared SWIFT_WARN_UNUSED_RESULT;
 - (nonnull instancetype)init OBJC_DESIGNATED_INITIALIZER;
 @end
 
