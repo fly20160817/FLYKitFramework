598284f59b9b5344f69b75f87e90863220aeddc8
“fly” <809176334@qq.com>
2025-11-13 16:07:03 +0800

    修改FLYRSA类的bug

--- FLYKit.framework/Headers/FLYDataConverter.h
+++ FLYKit.framework/Headers/FLYDataConverter.h
@@ -1,6 +1,6 @@
 //
 //  FLYDataConverter.h
-//  test111111
+//  FLYKit
 //
 //  Created by fly on 2025/11/13.
 //
@@ -12,45 +12,57 @@ NS_ASSUME_NONNULL_BEGIN
 @interface FLYDataConverter : NSObject
 
 /// 字符串 -> NSData
-+ (NSData *)dataFromString:(NSString *)string;
++ (NSData *)dataFromString:(NSString *)string
+NS_SWIFT_NAME(data(fromString:));
 
 /// 字典 -> NSData
-+ (NSData *)dataFromDictionary:(NSDictionary *)dictionary;
++ (NSData *)dataFromDictionary:(NSDictionary *)dictionary
+NS_SWIFT_NAME(data(fromDictionary:));
 
 /// 数组 -> NSData
-+ (NSData *)dataFromArray:(NSArray *)array;
++ (NSData *)dataFromArray:(NSArray *)array
+NS_SWIFT_NAME(data(fromArray:));
 
 
 /// NSData -> 字符串
-+ (NSString *)stringFromData:(NSData *)data;
++ (NSString *)stringFromData:(NSData *)data
+NS_SWIFT_NAME(string(fromData:));
 
 /// 字典 -> 字符串
-+ (NSString *)stringFromDictionary:(NSDictionary *)dictionary;
++ (NSString *)stringFromDictionary:(NSDictionary *)dictionary
+NS_SWIFT_NAME(string(fromDictionary:));
 
 /// 数组 -> 字符串
-+ (NSString *)jsonStringFromArray:(NSArray *)array;
++ (NSString *)stringFromArray:(NSArray *)array
+NS_SWIFT_NAME(string(fromArray:));
 
 
 /// NSData -> 数组
-+ (NSArray *)arrayFromData:(NSData *)data;
++ (NSArray *)arrayFromData:(NSData *)data
+NS_SWIFT_NAME(array(fromData:));
 
 /// 字符串 -> 数组
-+ (NSArray *)arrayFromString:(NSString *)string;
++ (NSArray *)arrayFromString:(NSString *)string
+NS_SWIFT_NAME(array(fromString:));
 
 
 /// NSData -> 字典
-+ (NSDictionary *)dictionaryFromData:(NSData *)data;
++ (NSDictionary *)dictionaryFromData:(NSData *)data
+NS_SWIFT_NAME(dictionary(fromData:));
 
 /// 字符串 -> 字典
-+ (NSDictionary *)dictionaryFromString:(NSString *)string;
++ (NSDictionary *)dictionaryFromString:(NSString *)string
+NS_SWIFT_NAME(dictionary(fromString:));
 
 
 
 /// NSData -> 十六进制字符串
-+ (NSString *)hexStringFromData:(NSData *)data;
++ (NSString *)hexStringFromData:(NSData *)data
+NS_SWIFT_NAME(hexString(fromData:));
 
 /// 十六进制字符串 -> NSData
-+ (NSData *)dataFromHexString:(NSString *)hexString;
++ (NSData *)dataFromHexString:(NSString *)hexString
+NS_SWIFT_NAME(data(fromHexString:));
 
 
 @end
